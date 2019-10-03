cd574fce3856616b8827947ab80c5748a02d1a0e
Kenny Quan <quan_kenny@yahoo.com>
2019-10-03 16:36:57 -0700

    Updated verson to 1.0.1

--- src/CloudFrontPurge.php
+++ src/CloudFrontPurge.php
@@ -73,7 +73,7 @@ class CloudFrontPurge extends Plugin
    *
    * @var string
    */
-  public $schemaVersion = '1.0.0';
+  public $schemaVersion = '1.0.1';
   public $invalidationSent = false;
 
   // Public Methods
