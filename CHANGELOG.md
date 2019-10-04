c52a025c61036ea5cae95685cb0078a49c3bc917
Kenny Quan <quan_kenny@yahoo.com>
2019-10-04 09:49:27 -0700

    Fixed casing, added back in config.php, added changes to changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,20 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## 1.0.0 - 2019-09-30
+## 1.0.1 - 2019-10-04
+
 ### Added
+
+- Initial release
+
+## 1.0.1 - 2019-10-03
+
+### Added
+
+- Initial release
+
+## 1.0.0 - 2019-09-30
+
+### Added
+
 - Initial release
---
+++ src/config.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * CloudFront Purge plugin for Craft CMS 3.x
+ *
+ * Cloud Front Purge
+ *
+ * @link      https://github.com/kayqq
+ * @copyright Copyright (c) 2019 Kenny Quan
+ */
+
+/**
+ * CloudFront Purge config.php
+ *
+ * This file exists only as a template for the CloudFront Purge settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'cloud-front-purge.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [];
